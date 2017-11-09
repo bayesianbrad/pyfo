bf749ae30ce156b6e2dcbb544eecf9ace791f12b
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-11-09 16:08:02 +0000

    sorting out framework

---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author: Bradley Gram-Hansen
+Time created:  12:41
+Date created:  09/11/2017
+
+License: MIT
+'''
+
+from os.path import realpath, dirname, join
+from setuptools import setup, find_packages
+import sys
+
+
+DISTNAME = 'pyfo'
+DESCRIPTION = "Python for FOPPL"
+LONG_DESCRIPTION =  open('README.txt').read()
+MAINTAINER = 'Bradley Gram-hansen, Yuan Zhang'
+MAINTAINER_EMAIL = 'bradleygramhansen@gmail.com'
+AUTHOR = 'Bradley Gram-Hansen and Yuan Zhang'
+AUTHOR_EMAIL = 'bradley@robots.ox.ac.uk'
+URL = "http://github.com/pyfo"
+LICENSE = 'LICENSE.txt'
+VERSION = "0.1.0"
+PACKAGES = ['DHMC', 'DHMC.tests', 'DHMC.src']
+classifiers = ['Development Status :: 1 - Production/UnStable',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: 3.5',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Science/Research',
+               'Topic :: Scientific/Engineering',
+               'Topic :: Scientific/Engineering :: Mathematics',
+               'Operating System :: OS Independent']
+
+PROJECT_ROOT = dirname(realpath(__file__))
+REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
+
+with open(REQUIREMENTS_FILE) as f:
+    install_reqs = f.read().splitlines()
+
+if sys.version_info < (3, 4):
+    install_reqs.append('enum34')
+
+# test_reqs = ['pytest', 'pytest-cov']
+# if sys.version_info[0] == 2:  # py3 has mock in stdlib
+#     test_reqs.append('mock')
+
+
+if __name__ == "__main__":
+    setup(name=DISTNAME,
+          version=VERSION,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          long_description=LONG_DESCRIPTION,
+          packages=find_packages(),
+          package_data={'docs': ['*']},
+          include_package_data=True,
+          classifiers=classifiers,
+          install_requires=install_reqs)
+          #
+          # tests_require=test_reqs,
+          # test_suite='nose.collector')
