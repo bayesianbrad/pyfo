e6ad2126dc189cac353381e6435f692fe06f2862
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-11-16 15:46:21 +0000

    New output from compiler

--- DHMC/foppl_py_interface.py
+++ DHMC/foppl_py_interface.py
@@ -1,40 +1,50 @@
 """
 A pseudo interface for now
 """
+#PYTHON 3  type hints.
+from DHMC.distributions_simples.distributions import *
+var_x_map = {x31020 0, [] 1} # need to modify output format
 
-def get_return_E(foppl-query)
-	return E
-	
-def get_prior_samples(foppl-query)
- 	return prior_samples
- 	
-def get_logp(xs)
-	return logp
-	
-def get_grad(xcont)
-	return grad_xcont
+# prior samples
+def gen_prior_samples():
+	dist31055 = Normal(mean=0, std=1)
+	x31020 = dist31055.sample()   #sample
+	x31057 = logical_trans( x31020 > 0)
+	dist31058 = Normal(mean=1, std=1)
+	y31023 = 1
+	x31060 = logical_trans( x31020 > 0)
+	x31061 = not logical_trans(x31060)
+	dist31062 = Normal(mean=-1, std=1)
+	y31026 = 1
+	Xs = gen_ordered_vars()
+	return Xs # need to modify output format
 
-def get_cont_var(xs)
-	"""
-	return continuous variable list
-	"""
-	return x_cont
-	
-def get_disc_var(xs)
-	"""
-	return discrete variable list
-	"""
-	return x_disc
-	
-	
-def get_discrete_var(xs)
-	"""
-	return variables from discrete distributions 
-	"""
-	return x_discrete
+# compute pdf
+def gen_pdf(Xs, compute_grad = True):
+	dist31055 = Normal(mean=0, std=1)
+	x31020 =  Xs[var_x_map.get('x31020')]   # get the x from input arg
+	p31056 = dist31055.logpdf( x31020) # from prior
+	x31057 = logical_trans( x31020 > 0)
+	dist31058 = Normal(mean=1, std=1)
+	y31023 = 1
+	p31059 = dist31058.logpdf( y31023) if x31057 else 0 # from observe with if
+	x31060 = logical_trans( x31020 > 0)
+	x31061 = not logical_trans(x31060)
+	dist31062 = Normal(mean=-1, std=1)
+	y31026 = 1
+	p31063 = dist31062.logpdf( y31026) if x31061 else 0 # from observe with if
+	logp =  p31056 + p31059 + p31063  # total log joint
+	var_cont = gen_cont_vars()
+	if compute_grad:
+		grad = torch.autograd.grad(logp, var_cont)[0] # need to modify format
+	return logp, grad # need to modify output format
 
-def get_var_map()
-	"""
-	map x gensym names with xs in dhmc
-	"""
-	return x_map
+def gen_ordered_vars():
+return [x31020,[]] # need to modify output format
+
+def gen_cont_vars():
+return [x31020] # need to modify output format
+
+def gen_disc_vars():
+return [[]] # need to modify output format
+
