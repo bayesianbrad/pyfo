36825328048eb173265b352698c305781a1b7f85
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-11-21 22:22:26 +0000

    Adding template for what user adds

--- DHMC/models/user_input.py
+++ DHMC/models/user_input.py
@@ -7,3 +7,11 @@ Date created:  20/11/2017
 
 License: MIT
 '''
+
+
+
+DHMC(<path_to_compiled_model>, n_samples, n_burnin, stepsize, trajectory_length, number_of_discrete_latents)
+
+
+noncont= {'Noncontinuous':}
+cont =   {'Continous':}
