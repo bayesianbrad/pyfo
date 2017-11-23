404cf48d5b7d08f380c23bfc0ace806c30271d2b
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-11-23 18:38:44 +0000

    template for a state

--- pyfo/models/state.py
+++ pyfo/models/state.py
@@ -7,24 +7,53 @@ Date created:  22/11/2017
 
 License: MIT
 '''
+import torch
+from torch.autograd import Variable
+from typing import Dict, List, Bool
+from pyfo.utils.core import VariableCast
 
 class State(object):
     """
-    Stores the state of the object in  a has map
+    Stores the state of the object in
 
     """
 
-    def __init__(self, foppy_out):
+    def __init__(self, gen_prior_samples, gen_pdf, gen_cont_vars, gen_disc_vars):
 
-        self._foppl_out = foppl_out #it should contain the latents and obvs
+        self.state = gen_prior_samples
+        self._gen_pdf = gen_pdf # returns logp
+        # TO DO: Tell Yuan not to only return logp
+        self._cont_vars = gen_cont_vars #includes the piecewise variables for now.
+        self._disc_vars = gen_disc_vars
 
-        # create intial state
 
-    def intiate(self):
+    def _intiate_state(self):
         """
         :param
         :return:
         """
-        state = ['params', 'obs']
-        state = {i: {} for i in state}
+        state = self._gen_pdf
         return state
+
+    def _to_Variable(self):
+        """
+        converts the vars of the state to floats, by extracting the data.
+        :param state
+        :return:
+        """
+    def _log_pdf(self, state):
+        """
+        This needs convert the state map
+
+        :param
+        :return: log_pdf
+        """
+
+    def _state_grad(self):
+        """
+        Calculates the gradient
+        :param compute_grad: type: Variable
+        :return: gradients
+        """
+        # grad = torch.autograd.grad(logp, var_cont)[0]  # need to modify format
+
