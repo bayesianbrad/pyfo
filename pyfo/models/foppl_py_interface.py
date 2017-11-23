95cc04af7aa8e1f119ae591c3de22fc8f709eb04
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-11-23 18:41:30 +0000

    need to change parameters to Variables

--- pyfo/models/foppl_py_interface.py
+++ pyfo/models/foppl_py_interface.py
@@ -3,9 +3,10 @@ A pseudo interface for now
 """
 #PYTHON 3  type hints.
 from pyfo.distributions import *
-
+from typing import Dict, List, Bool
+from torch.autograd import Variable
 # prior samples
-def gen_prior_samples():
+def gen_prior_samples() -> Dict[str, Variable]:
 	dist31055 = Normal(mu=0, sigma=1)
 	x31020 = dist31055.sample()   #sample
 	x31057 = logical_trans( x31020 > 0)
@@ -15,27 +16,10 @@ def gen_prior_samples():
 	x31061 = not logical_trans(x31060)
 	dist31062 = Normal(mean=-1, std=1)
 	y31026 = 1
-	Xs = gen_ordered_vars()
-
-	# if Xs is a list of strings, then using the following makes a list of dict.fromkeys(Xs) {'l1':None, 'l2':None ....} Etc
-
-	dic = dict.fromkeys(Xs)
-	for k1 in state.keys():
-		for k2 in state[k1]:
-			print(' This is the key {0} for the main state and this is the subkey {1} with value {2}'.format(k1, k2,
-																											 state[k1][
-																												 k2]))
-
-	#Ideally we would have First hashmap : {'params': {},'obs': {}}
-	# then populate it with
-
-	# state  = {'params': {'x1':<val>, 'x2':<val> , ...},'obs':{y31023:<val>}}
-	#  ''
-
-	return Xs # need to modify output format
+	return Xs# need to modify output format
 
 # compute pdf
-def gen_pdf(Xs, compute_grad = True):
+def gen_pdf(Xs: Dict, compute_grad: bool = True):
 	dist31055 = Normal(mean=0, std=1)
 	x31020 =  Xs[var_x_map.get('x31020')]   # get the x from input arg
 	p31056 = dist31055.logpdf( x31020) # from prior
@@ -50,8 +34,6 @@ def gen_pdf(Xs, compute_grad = True):
 	p31063 = dist31062.logpdf( y31026) if x31061 else 0 # from observe with if
 	logp =  p31056 + p31059 + p31063  # total log joint
 	var_cont = gen_cont_vars()
-	if compute_grad:
-		grad = torch.autograd.grad(logp, var_cont)[0] # need to modify format
 	return logp, grad # need to modify output format
 
 def gen_cont_vars():
