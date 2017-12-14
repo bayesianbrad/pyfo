f0435b7c3e919f315a9ce4a38e22475a71b26095
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-12-14 21:31:39 +0000

    the interface for testing continuous parameters

--- pyfo/models/cont_interface_test.py
+++ pyfo/models/cont_interface_test.py
@@ -2,7 +2,13 @@ import pyfo.distributions as dist
 import pyfo.inference as interface
 
 class model(interface):
-
+    """
+    For Yuan:
+    Make sure gen_vars, _cont, _disc_vars are all outputting lists of strings
+    When calling logpdf, write as log_pdf
+    mean and std to be replaced by mu and sigma.
+    For other distributions look at the distribution classes for details.
+    """
     def __init__(self):
         super().__init__()
 
@@ -17,10 +23,10 @@ class model(interface):
 
     # prior samples
     def gen_prior_samples(self):
-        dist32896 = dist.Normal(mean=1.0, std=5.0)
+        dist32896 = dist.Normal(mu=1.0, sigma=5.0)
         x32870 = dist32896.sample()   #sample
         x32898 =  x32870 + 1
-        dist32899 = dist.Normal(mean=x32898, std=2.0)
+        dist32899 = dist.Normal(mu=x32898, sigma=2.0)
         y32871 = 7.0
 
         return [x32870]
@@ -28,13 +34,13 @@ class model(interface):
     # compute pdf
     def gen_pdf(self,x):
         # Notes: At this point, all x's need to be leaf nodes
-        dist32896 = dist.Normal(mean=1.0, std=5.0)
+        dist32896 = dist.Normal(mu=1.0, sigma=5.0)
         x32870 =  x['x32870']   # get the x from input arg
-        p32897 = dist32896.logpdf( x32870) # from prior
+        p32897 = dist32896.log_pdf(x32870) # from prior
         x32898 =  x32870 + 1
-        dist32899 = dist.Normal(mean=x32898, std=2.0)
+        dist32899 = dist.Normal(mu=x32898, sigma=2.0)
         y32871 = 7.0
-        p32900 = dist32899.logpdf( y32871) # from observe
+        p32900 = dist32899.log_pdf( y32871) # from observe
         logp =  p32897 + p32900  # total log joint
         return logp # need to modify output format
 
