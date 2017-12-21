06daad6d30612d8e69898f5f582ac9994c4525e3
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-12-21 17:03:29 +0000

    debug script

--- pyfo/debugging/init_run.py
+++ pyfo/debugging/init_run.py
@@ -56,14 +56,16 @@ The loader method has a few responsibilities to filL:
 
 '''
 
-from pyfo.models.simple import model
+from pyfo.models.ifsimp import model
 from pyfo.inference.dhmc import DHMCSampler as dhmc
 dhmc_ = dhmc(model)
-burn_in = 1000
-n_sample = 10 ** 4
+burn_in = 100
+n_sample = 10 ** 3
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
 
 stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range)
 samples = stats['samples']
-print(samples)
+means = stats['means']
+print(means)
+print(stats['accept_prob'])
