9993bbe164dab5f7f32e807e17bf7b948422c15d
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-12-29 13:50:23 +0000

    updated the gitignore file

---
+++ tests/test5.py
@@ -0,0 +1,10 @@
+from py_foppl.foppl import imports
+import py_foppl.test_if_src as test_if_src
+
+# print(dir(test_if_src))
+# print("=" * 50)
+print(test_if_src.code)
+print("=" * 50)
+# print(test_if_src.graph)
+# print("=" * 50)
+# print(help(test_if_src.model))
