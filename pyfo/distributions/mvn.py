bd9e72e292a1bf02e9b9fd2c486512d4d270bff2
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-04 15:52:30 +0000

    added full multivariate normal class from pyfo and wrapped variablecast

--- pyfo/distributions/mvn.py
+++ pyfo/distributions/mvn.py
@@ -7,158 +7,157 @@ Date created:  13/11/2017
 
 License: MIT
 '''
+
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import torch
 from torch.autograd import Variable
-
 from pyfo.distributions.distribution import Distribution
 from pyfo.utils.core import VariableCast
 
 
+def potri_compat(var):
+    return Variable(torch.potri(var.data)) if torch.__version__ < '0.3.0' else torch.potri(var)
+
+
+def potrf_compat(var):
+    return Variable(torch.potrf(var.data)) if torch.__version__ < '0.3.0' else torch.potrf(var)
+
+
+def linear_solve_compat(matrix, matrix_chol, y):
+    """Solves the equation ``torch.mm(matrix, x) = y`` for x."""
+    assert matrix.requires_grad == matrix_chol.requires_grad
+    if matrix.requires_grad or y.requires_grad:
+        # If derivatives are required, use the more expensive gesv.
+        return torch.gesv(y, matrix)[0]
+    else:
+        # Use the cheaper Cholesky solver.
+        return torch.potrs(y, matrix_chol)
+
+
+def matrix_inverse_compat(matrix, matrix_chol):
+    """Computes the inverse of a positive semidefinite square matrix."""
+    assert matrix.requires_grad == matrix_chol.requires_grad
+    if matrix.requires_grad:
+        # If derivatives are required, use the more expensive inverse.
+        return torch.inverse(matrix)
+    else:
+        # Use the cheaper Cholesky based potri.
+        return potri_compat(matrix_chol)
+
+
 class MultivariateNormal(Distribution):
+    """Multivariate normal (Gaussian) distribution.
+    A distribution over vectors in which all the elements have a joint Gaussian
+    density.
+    :param torch.autograd.Variable loc: Mean. Must be a vector (Variable
+        containing a 1d Tensor).
+    :param torch.autograd.Variable covariance_matrix: Covariance matrix.
+        Must be symmetric and positive semidefinite.
+    :param torch.autograd.Variable scale_tril: The Cholesky decomposition of
+        the covariance matrix. You can pass this instead of `covariance_matrix`.
+    :param use_inverse_for_batch_log: If this is set to true, the torch.inverse
+        function will be used to compute log_pdf. This means that the results of
+        log_pdf can be differentiated with respect to the covariance matrix.
+        Since the gradient of torch.potri is currently not implemented,
+        differentiation of log_pdf wrt. covariance matrix is not possible when
+        using the Cholesky decomposition. Using the Cholesky decomposition is
+        however much faster and therefore enabled by default.
+    :param normalized: If set to `False` the normalization constant is omitted
+        in the results of batch_log_pdf and log_pdf. This might be preferable,
+        as computing the determinant of the covariance matrix might not always
+        be numerically stable. Defaults to `True`.
+    :raises: ValueError if the shape of any parameter is not supported.
     """
-    Univariate normal (Gaussian) distribution.
 
-    A distribution over tensors in which each element is independent and
-    Gaussian distributed, with its own mean and standard deviation. The
-    distribution is over tensors that have the same shape as the parameters `mu`
-    and `sigma`, which in turn must have the same shape as each other.
-
-    This is often used in conjunction with `torch.nn.Softplus` to ensure the
-    `sigma` parameters are positive.
-
-    :param torch.autograd.Variable mu: Means.
-    :param torch.autograd.Variable sigma: Standard deviations.
-        Should be positive and the same shape as `mu`.
-    """
     reparameterized = True
 
-    def __init__(self, mu, cov, batch_size=None, log_pdf_mask=None, *args, **kwargs):
-        self.mu = VariableCast(mu)
-        self.cov = VariableCast(cov)
-        self.log_pdf_mask = log_pdf_mask
-        assert self.mean.data.size()[0] == self.cov.size()[0]  # , "ERROR! mean and cov have different size!")
-        self.chol_std = torch.t(torch.potrf(self.cov)) # lower triangle
-        self.chol_std_inv = torch.inverse(self.chol_std) # cholesky decompositon
-        if self.mu.size()[1] != self.cov.size()[1]:
-            raise ValueError("Expected mu.size()[1] == sigma.size()[1], but got {} vs {}".format(mu.size()[1], cov.size()[1]))
-        if self.mu.dim() == 1 and batch_size is not None:
-            self.mu = self.mu.expand(batch_size, mu.size(0))
-            self.sigma = self.cov.expand(batch_size, cov.size(0))
-            if log_pdf_mask is not None and log_pdf_mask.dim() == 1:
-                self.log_pdf_mask = log_pdf_mask.expand(batch_size, log_pdf_mask.size(0))
+    def __init__(self, loc, covariance_matrix=None, scale_tril=None, batch_size=None, use_inverse_for_batch_log=False,
+                 normalized=True, *args, **kwargs):
+        if covariance_matrix is None and scale_tril is None:
+            raise ValueError('At least one of covariance_matrix or scale_tril must be specified')
+        if covariance_matrix is not None:
+            cov_matrix  = VariableCast(covariance_matrix)
+        if scale_tril is not None:
+            scale_tri = VariableCast(scale_tril)
+        self.mu = VariableCast(loc)
+        self.output_shape = self.mu.size()
+        self.use_inverse_for_batch_log = use_inverse_for_batch_log
+        self.normalized = normalized
+        self.batch_size = batch_size if batch_size is not None else 1
+        if scale_tril is not None and covariance_matrix is not None:
+            raise ValueError("Only either 'covariance_matrix' or 'scale_tril' can be passed at once.")
+        if cov_matrix is not None:
+            if not cov_matrix.dim() == 2:
+                raise ValueError("The covariance matrix must be a matrix, but got covariance_matrix.size() = {}".format(
+                    self.mu.size()))
+            self.sigma = VariableCast(cov_matrix)
+            self.sigma_cholesky = potrf_compat(cov_matrix)
+        else:
+            if not scale_tri.dim() == 2:
+                raise ValueError("The Cholesky decomposition of the covariance matrix must be a matrix, "
+                                 "but got scale_tril.size() = {}".format(
+                                     self.mu.size()))
+            self.sigma = torch.mm(scale_tri.transpose(0, 1), scale_tri)
+            self.sigma_cholesky = scale_tri
+        if self.mu.dim() > 1:
+            raise ValueError("The mean must be a vector, but got loc.size() = {}".format(self.mu.size()))
+
         super(MultivariateNormal, self).__init__(*args, **kwargs)
 
     def batch_shape(self, x=None):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.batch_shape`
-        """
-        event_dim = 1
-        mu = self.mu
-        if x is not None:
-            if x.size()[-event_dim] != mu.size()[-event_dim]:
+        mu = self.mu.expand(self.batch_size, *self.mu.size()).squeeze(0)
+        X = VariableCast(x)
+        if X is not None:
+            if X.size()[-1] != mu.size()[-1]:
                 raise ValueError("The event size for the data and distribution parameters must match.\n"
-                                 "Expected x.size()[-1] == self.mu.size()[-1], but got {} vs {}".format(
-                                     x.size(-1), mu.size(-1)))
+                                 "Expected x.size()[-1] == self.mu.size()[0], but got {} vs {}".format(
+                                     X.size(-1), mu.size(-1)))
             try:
-                mu = self.mu.expand_as(x)
+                mu = mu.expand_as(X)
             except RuntimeError as e:
                 raise ValueError("Parameter `mu` with shape {} is not broadcastable to "
                                  "the data shape {}. \nError: {}".format(mu.size(), x.size(), str(e)))
-        return mu.size()[:-event_dim]
+
+        return mu.size()[:-1]
 
     def event_shape(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.event_shape`
-        """
-        event_dim = 1
-        return self.mu.size()[-event_dim:]
+        return self.mu.size()[-1:]
 
     def sample(self):
-        """
-        Reparameterized Normal sampler.
-
+        """Generate a sample with the specified covariance matrix and mean.
+        Differentiation wrt. to the covariance matrix is only supported on
+        PyTorch version 0.3.0 or higher.
         Ref: :py:meth:`pyro.distributions.distribution.Distribution.sample`
         """
-        eps = Variable(torch.randn(self.mu.size()).type_as(self.mu.data))
-        z = self.mu + eps * self.sigma
-        return z if self.reparameterized else z.detach()
+        batch_size = self.batch_size
+        uncorrelated_standard_sample = Variable(torch.randn(
+            batch_size, *self.mu.size()).type_as(self.mu.data))
+        transformed_sample = self.mu + \
+            torch.mm(uncorrelated_standard_sample, self.sigma_cholesky)
+        return transformed_sample if self.reparameterized else transformed_sample.detach()
 
     def batch_log_pdf(self, x):
-        """
-        Diagonal Normal log-likelihood
+        if not self.normalized and self.sigma_cholesky.requires_grad:
+            raise NotImplementedError("Differentiation is not supported (yet) if normalized=False.")
+        X =VariableCast(x)
 
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.batch_log_pdf`
-        """
-        # expand to patch size of input
-        x = VariableCast(x)
-        mu = self.mu.expand(self.shape(x))
-        sigma = self.sigma.expand(self.shape(x))
-        log_pxs = -1 * (torch.log(sigma) + 0.5 * np.log(2.0 * np.pi) + 0.5 * torch.pow((x - mu) / sigma, 2))
-        # XXX this allows for the user to mask out certain parts of the score, for example
-        # when the data is a ragged tensor. also useful for KL annealing. this entire logic
-        # will likely be done in a better/cleaner way in the future
-        if self.log_pdf_mask is not None:
-            log_pxs = log_pxs * self.log_pdf_mask
-        batch_log_pdf = torch.sum(log_pxs, -1)
-        batch_log_pdf_shape = self.batch_shape(x) + (1,)
-        return batch_log_pdf.contiguous().view(batch_log_pdf_shape)
+        batch_size = X.size()[0] if len(X.size()) > len(self.mu.size()) else 1
+        batch_log_pdf_shape = self.batch_shape(X) + (1,)
+        X = X.view(batch_size, *self.mu.size())
+        # TODO Implement the gradient of the normalization factor if normalized=False
+        normalization_factor = torch.log(
+            self.sigma_cholesky.diag()).sum() + (self.mu.size()[0] / 2) * np.log(2 * np.pi) if self.normalized else 0
+        # TODO It may be useful to switch between matrix_inverse_compat() and linear_solve_compat() based on the
+        # batch size and the size of the covariance matrix
+        sigma_inverse = matrix_inverse_compat(self.sigma, self.sigma_cholesky)
+        return -(normalization_factor + 0.5 * torch.sum((X - self.mu).unsqueeze(2) * torch.bmm(
+            sigma_inverse.expand(batch_size, *self.sigma_cholesky.size()),
+            (X - self.mu).unsqueeze(-1)), 1)).view(batch_log_pdf_shape)
 
     def analytic_mean(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.analytic_mean`
-        """
         return self.mu
 
     def analytic_var(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.analytic_var`
-        """
-        return torch.pow(self.sigma, 2)
-
-    def is_discrete(self):
-        """
-            Ref: :py:meth:`pyro.distributions.distribution.Distribution.is_discrete`.
-        """
-        return False
-
-
-
-    #Our MVN
-    # class MultivariateNormal():
-    #     """Normal random variable"""
-    #
-    #     def __init__(self, mean, cov):
-    #         """Initialize this distribution with mean, cov.
-    #         input:
-    #             mean: n by 1
-    #             cov: covariance matrix, n by n
-    #         """
-    #         self.mean = VariableCast(mean)
-    #         self.cov = VariableCast(cov)
-    #         assert self.mean.data.size()[0] == self.cov.data.size()[0]  # , "ERROR! mean and cov have different size!")
-    #         self.dim = self.mean.data.size()[0]
-    #         self.chol_std = VariableCast(torch.t(torch.potrf(self.cov.data)))  # lower triangle
-    #         self.chol_std_inv = torch.inverse(self.chol_std)
-    #
-    #     def sample(self, num_samples=1):
-    #         zs = torch.randn(self.dim, 1)
-    #         # print("zs", zs)
-    #         samples = Variable(self.mean.data + torch.matmul(self.chol_std.data, zs), requires_grad=True)
-    #         return samples
-    #
-    #     def logpdf(self, value):
-    #         """
-    #         value : obs value, should be n by 1
-    #         :return: scalar, log pdf value
-    #         """
-    #         value = VariableCast(value)
-    #         cov_det = self.chol_std.diag().prod() ** 2
-    #         log_norm_constant = 0.5 * self.dim * torch.log(torch.Tensor([2 * np.pi])) \
-    #                             + 0.5 * torch.log(cov_det.data)
-    #         right = torch.matmul(self.chol_std_inv, value - self.mean)
-    #         # print(value, self.mean, value - self.mean)
-    #         log_p = - Variable(log_norm_constant) - 0.5 * torch.matmul(torch.t(right), right)
-    #         return log_p
+        return torch.diag(self.sigma)    #         return log_p
