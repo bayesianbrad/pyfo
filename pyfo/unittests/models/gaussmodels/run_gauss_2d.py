8d71ca648a524dce07cb64c42b030f8962f94986
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-15 14:31:54 +0000

    Models will now run and save samples, for each parameter

--- pyfo/unittests/models/gaussmodels/run_gauss_2d.py
+++ pyfo/unittests/models/gaussmodels/run_gauss_2d.py
@@ -6,12 +6,12 @@ from pyfo.inference.dhmc import DHMCSampler as dhmc
 from pyfo.utils.eval_stats import *
 
 dhmc_ = dhmc(model)
-burn_in = 10 ** 3
-n_sample = 10 ** 3
+burn_in = 10 ** 1
+n_sample = 10 ** 2
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
 
-stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range)
+stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range, save_samples= True)
 
 samples =  stats['samples']
 all_samples = stats['samples_wo_burin'] # type, panda dataframe
--- pyfo/unittests/models/ifmodels/run_if_1d.py
+++ pyfo/unittests/models/ifmodels/run_if_1d.py
@@ -3,16 +3,17 @@ from pyfo.inference.dhmc import DHMCSampler as dhmc
 from pyfo.utils.eval_stats import *
 
 dhmc_ = dhmc(model)
-burn_in = 10 ** 3
-n_sample = 10 ** 3
+burn_in = 10 ** 1
+n_sample = 10 ** 2
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
 
-stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range)
+stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range, print_stats=True, save_samples=True)
 
 samples =  stats['samples']
 all_samples = stats['samples_wo_burin'] # type, panda dataframe
 
-print('mean_samples: ', extract_means(samples) , '\n')
-print('mean_all_samples: ', extract_means(all_samples) , '\n')
+# print('mean_samples: ', extract_means(samples, 'x') , '\n')
+# print('mean_samples w/o key: ', extract_means(samples) , '\n')
+# print('mean_all_samples: ', extract_means(all_samples, 'x') , '\n')
 # print(samples)
