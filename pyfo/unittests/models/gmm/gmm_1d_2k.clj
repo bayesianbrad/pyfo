9bb29469454d4d5ae1530660d556afcfc01d649b
Yuan Zhou <yuan.zhou@cs.ox.ac.uk>
2018-01-20 13:13:44 +0000

    correct model

--- pyfo/unittests/models/gmm/gmm_1d_2k.clj
+++ pyfo/unittests/models/gmm/gmm_1d_2k.clj
@@ -1,5 +1,11 @@
+;(let [mu (vector -5 5)
+;      obs (vector -7 7)
+;      z (sample (categorical [0.3 0.7]))]
+;  (observe (normal (get mu z) 2) (get obs z))
+;  (vector z (get mu z)))
+
 (let [mu (vector -5 5)
-      obs (vector -7 7)
-      z (sample (categorical [0.3 0.7]))]
-  (observe (normal (get mu z) 2) (get obs z))
-  (vector z (get mu z)))
+      obs 7
+      z (sample (categorical [0.5 0.5]))]
+  (observe (normal (get mu z) 2) obs)
+  (vector z mu))
