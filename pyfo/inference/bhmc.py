76f63bd9858f40543cf57e0f3cda732915a98b0e
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-23 16:04:48 +0000

    BHMC interface updated

--- pyfo/inference/bhmc.py
+++ pyfo/inference/bhmc.py
@@ -33,7 +33,7 @@ from pyfo.utils.eval_stats import extract_stats
 from pyfo.utils.eval_stats import save_data
 from pyfo.utils.plotting import Plotting as plot
 
-class DHMCSampler(object):
+class BHMCSampler(object):
     """
     In general model will be the output of the foppl compiler, it is not entirely obvious yet where this
     will be stored. But for now, we will inherit the model from pyro.models.<model_name>
@@ -45,34 +45,40 @@ class DHMCSampler(object):
         ## state is a class that contains a dictionary of the system.
         ## to get log_posterior and log_update call the methods on the
         ## state
-        # Note:
-        ## Need to deal with a M matrix. Using the identity matrix for now.
+        # 2nd Note:
+        ## Rather than dealing with the 'indicies' of parameters we deal instead with the keys
+        # 3rd Note
+        ## When generating the mometum dictionary we need the discrete params to have their
+        ## momentum drawn from the laplacian, and the continuous params to be drawn from
+        ## gaussian
+        #4 TH NOTE
+        ## Need to deal with a M matrix. I may  just set it to 1 everywhere, inferring the identity.
 
-        self.model_graph =object.model # i graphical model object
-        self._state = state.State(self.model_graph)
+        self.model_graph =object # instantiates model
+        self._state = state.State(object.model())
 
-        ## Debugging:::
-        #####
-        self._state.debug()
         # Parameter keys
         self._disc_keys = self._state._return_disc_list()
         self._cont_keys = self._state._return_cont_list()
         self._cond_keys = self._state._return_cond_list()
-        self._if_keys = self._state._return_if_list()
+
+        ################
+        # Do not exist in state yet
+        self._if_disc_keys = self._state._return_if_disc_list()
+        self._if_cont_keys = self._state._return_if_cont_list()
+        ##################
+        self._all_keys = self._state._return_all_list()
+
         # True latent variable names
-        self._names = self._state.get_original_names()
+        self._names = self._state._return_true_names()
 
         self.grad_logp = self._state._grad_logp
         self.init_state = self._state.intiate_state() # this is just x0
-        # self.n_disc = len(self._disc_keys)
-        # self.n_cont = len(self._cont_keys)
-        # self.n_params =  self.n_disc + self.n_cont
         self.M = 1 #Assumes the identity matrix and assumes everything is 1d for now.
 
         self.log_posterior = self._state._log_pdf  # returns a function that is the current state as argument
-        # self.logp_update = self._state._log_pdf_update # returns a function ; not currently used 10:50 14th Dec.
 
-    def random_momentum(self):
+    def random_momentum(self, branch_trig=False):
         """
         Constructs a momentum dictionary where for the discrete keys we have laplacian momen tum
         and for continous keys we have gaussian
@@ -81,14 +87,16 @@ class DHMCSampler(object):
         p = {}
         if self._disc_keys is not None:
             for key in self._disc_keys:
-                p[key] = self.M * VariableCast(np.random.laplace(size=1)) #in the future add support for multiple dims
+                p[key] = self.M * VariableCast(np.random.laplace(size=1))  # in the future add support for multiple dims
         if self._cont_keys is not None:
             for key in self._cont_keys:
-                p[key] = VariableCast(self.M * torch.normal(torch.FloatTensor([0]),torch.FloatTensor([1])))
+                p[key] = VariableCast(self.M * torch.normal(torch.FloatTensor([0]), torch.FloatTensor([1])))
                 # TODO in the future make for multiple dims
-        if self._if_keys is not None:
+        if branch_trig:
             for key in self._if_keys:
                 p[key] = self.M * VariableCast(np.random.laplace(size=1))
+            if self._if_keys is not None:
+                p[key] = VariableCast(self.M * torch.normal(torch.FloatTensor([0]), torch.FloatTensor([1])))
         return p
 
     def coordInt(self,x,x_embed,p,stepsize,key, unembed=False):
@@ -123,17 +131,30 @@ class DHMCSampler(object):
             p[key] = -p[key]
             return x_embed[key],p[key], 0
 
-    def gauss_laplace_leapfrog(self, x0, p0, stepsize, aux= None):
+    def append_keys(self, if_cont=False, if_disc=False):
+        if if_cont:
+            if self._if_cont_keys is not None:
+                if self._cont_keys is not None:
+                    self._cont_keys = self._cont_keys.append(self._if_cont_keys)
+        if if_disc:
+            if self._if_disc_keys is not None:
+                if self._disc_keys is not None:
+                    temp_disc_keys = self._disc_keys
+                    return temp_disc_keys.append(self._if_disc_keys)
+        if if_cont is False:
+            self._cont_keys = [key for key in self._cont_keys not in self._if_cont_keys]
+        if if_disc is False:
+            self._disc_keys = [key for key in self._disc_keys not in self._if_disc_keys]
+
+    def branching_integrator(self, x0, p0, stepsize, t):
         """
         Performs the full DHMC update step. It updates the continous parameters using
         the standard integrator and the discrete parameters via the coordinate wie integrator.
 
         :param x: Type dictionary
         :param p: Type dictionary
-        :param stepsize:
-        :param log_grad:
-        :param aux:
-        :param n_disc:
+        :param stepsize: Type: Variable
+        :param t Type: int trajectory number
         :return: x, p the proposed values as dict.
         """
 
@@ -141,21 +162,25 @@ class DHMCSampler(object):
         n_feval = 0
         n_fupdate = 0
 
-        #performs shallow copy
+        # performs shallow copy
         x = copy.copy(x0)
         p = copy.copy(p0)
+
+        if t is 0:
+            self.append_keys(if_cont=True, if_disc=True)
+
         # perform first step of leapfrog integrators
         if self._cont_keys is not None:
             logp = self.log_posterior(x, set_leafs=True)
             for key in self._cont_keys:
-                p[key] = p[key] + 0.5*stepsize*self.grad_logp(logp,x[key])
+                p[key] = p[key] + 0.5 * stepsize * self.grad_logp(logp, x[key])
 
         if self._disc_keys is None and self._if_keys is None:
             for key in self._cont_keys:
-                x[key] = x[key] + stepsize*self.M * p[key] # full step for postions
+                x[key] = x[key] + stepsize * self.M * p[key]  # full step for postions
             logp = self.log_posterior(x, set_leafs=True)
             for key in self._cont_keys:
-                p[key] = p[key] + 0.5*stepsize*self.grad_logp(logp,x[key])
+                p[key] = p[key] + 0.5 * stepsize * self.grad_logp(logp, x[key])
             return x, p, n_feval, n_fupdate, 0
 
         else:
@@ -164,7 +189,7 @@ class DHMCSampler(object):
                 permuted_keys_list = permuted_keys_list + self._disc_keys
             if self._if_keys is not None:
                 permuted_keys_list = permuted_keys_list + self._if_keys
-            permuted_keys = permutations(permuted_keys_list,1)
+            permuted_keys = permutations(permuted_keys_list, 1)
             # permutates all keys into one permutated config. It deletes in memory as each key is called
             # returns a tuple ('key', []), hence to call 'key' requires [0] index.
 
@@ -177,21 +202,21 @@ class DHMCSampler(object):
                 #       'print the permuted_key : {0} \n'
                 #       'and key[0]: {1}'.format(key, key[0]))
                 if self._if_keys is not None and key[0] in self._if_keys:
-                    x[key[0]], p[key[0]], _ = self.coordInt(x,x_embed, p, stepsize, key[0], unembed=False)
+                    x[key[0]], p[key[0]], _ = self.coordInt(x, x_embed, p, stepsize, key[0], unembed=False)
                 else:
-                    x[key[0]], p[key[0]], _ = self.coordInt(x,x_embed, p, stepsize, key[0], unembed=True)
+                    x[key[0]], p[key[0]], _ = self.coordInt(x, x_embed, p, stepsize, key[0], unembed=True)
                 if math.isinf(_):
                     return x0, p, n_feval, n_fupdate, _
             n_fupdate += 1
 
             if self._cont_keys is not None:
                 for key in self._cont_keys:
-                    x[key] = x[key] + 0.5 * stepsize * self.M * p[key] # final half step for position
+                    x[key] = x[key] + 0.5 * stepsize * self.M * p[key]  # final half step for position
 
             if self._cont_keys is not None:
                 logp = self.log_posterior(x, set_leafs=True)
                 for key in self._cont_keys:
-                    p[key] = p[key] + 0.5*stepsize*self.grad_logp(logp, x[key]) # final half step for momentum
+                    p[key] = p[key] + 0.5 * stepsize * self.grad_logp(logp, x[key])  # final half step for momentum
                 n_feval += 1
             return x, p, n_feval, n_fupdate, 0
 
@@ -237,7 +262,7 @@ class DHMCSampler(object):
         n_fupdate = 0
         x = copy.copy(x0)
         for i in range(n_step):
-            x,p, n_feval_local, n_fupdate_local, _ = self.gauss_laplace_leapfrog(x,p,stepsize)
+            x,p, n_feval_local, n_fupdate_local, _ = self.branching_integrator(x,p,stepsize,t=i)
             if math.isinf(_):
                 break
             n_feval += n_feval_local
