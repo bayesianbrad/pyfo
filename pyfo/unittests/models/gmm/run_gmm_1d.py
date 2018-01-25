4138f4000a12bf536b586878454ac15ef5f7c12e
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-25 21:02:05 +0000

    added more models

--- pyfo/unittests/models/gmm/run_gmm_1d.py
+++ pyfo/unittests/models/gmm/run_gmm_1d.py
@@ -8,7 +8,7 @@ Date created:  19/01/2018
 License: MIT
 '''
 from pyfo.pyfoppl.foppl import imports
-import pyfo.unittests.models.gmm.gmm_1d_b as test
+import pyfo.unittests.models.gmm.gmm_1d_c as test
 
 
 # print(test.code)
@@ -16,14 +16,13 @@ from pyfo.inference.dhmc import DHMCSampler as dhmc
 
 dhmc_ = dhmc(test)
 
-burn_in = 1000
-n_sample = 1000
+burn_in = 1
+n_sample = 1
 stepsize_range = [0.03,0.15]
 n_step_range = [1, 2]
-
-stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range,plot=True, print_stats=True, save_samples=True)
-
-samples =  stats['samples']
-all_samples = stats['samples_wo_burin'] # type, panda dataframe
-print(stats['accept_prob'])
 test.model.display_graph()
+# stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range,plot=False, print_stats=True, save_samples=True)
+#
+# samples =  stats['samples']
+# all_samples = stats['samples_wo_burin'] # type, panda dataframe
+# print(stats['accept_prob'])
--- pyfo/unittests/models/hmm/run_hmm.py
+++ pyfo/unittests/models/hmm/run_hmm.py
@@ -9,7 +9,7 @@ License: MIT
 '''
 
 from pyfo.pyfoppl.foppl import imports
-import hmm as test
+import pyfo.unittests.models.hmm.hmm as test
 
 from pyfo.inference.dhmc import DHMCSampler as dhmc
 
@@ -18,11 +18,10 @@ burn_in =100
 n_sample = 100
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
-
+test.model.display_graph()
 stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range, print_stats=True, save_samples=True)
 
 samples =  stats['samples']
 all_samples = stats['samples_wo_burin'] # type, panda dataframe
 print(stats['accept_prob'])
 
-test.model.display_graph()
