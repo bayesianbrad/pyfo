47cb89a52061a63544d8f3cc3c3dcf5519c9657b
Yuan Zhou <yuan.zhou@cs.ox.ac.uk>
2018-01-25 01:01:43 +0000

    update lr model

--- pyfo/unittests/models/gaussmodels/run_gauss_1d.py
+++ pyfo/unittests/models/gaussmodels/run_gauss_1d.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from pyfo.pyfoppl.foppl import imports
-import if_1d as test
+import pyfo.unittests.models.gauss_1d as test
 from pyfo.inference.dhmc import DHMCSampler as dhmc
 from pyfo.utils.eval_stats import *
 # print(test.code)
 dhmc_ = dhmc(test)
-burn_in = 10 ** 1
+burn_in = 10 ** 3
 n_sample = 10 ** 3
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
@@ -21,3 +21,14 @@ all_samples = stats['samples_wo_burin'] # type, panda dataframe
 # means = stats['means']
 # print(means)
 print(stats['accept_prob'])
+
+
+x = samples.as_matrix(columns=['x'])
+t,d = x.shape
+x_ = np.empty(shape=(1,t,d))
+x_[0] = x
+mu_true = 5.28
+std_true = 1.429
+ess = effective_sample_size(x_, [mu_true], [std_true**2])
+print(ess)
+print('acceptace rate: ', acceptance_rate(x_) )
