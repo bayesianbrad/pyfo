429e189c639d11d5000e8580ebdad97b4f4eec7c
Yuan Zhou <yuan.zhou@cs.ox.ac.uk>
2018-01-26 21:38:03 +0000

    update gauss models

--- pyfo/unittests/models/gaussmodels/run_gauss_1d.py
+++ pyfo/unittests/models/gaussmodels/run_gauss_1d.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from pyfo.pyfoppl.foppl import imports
-import pyfo.unittests.models.mvn as test
 from pyfo.inference.dhmc import DHMCSampler as dhmc
+import pyfo.unittests.models.gaussmodels.gauss_1d as test
 from pyfo.utils.eval_stats import *
-# print(test.code)
+
+# model
+print(test.model)
+test.model.display_graph()
+
+# inference
 dhmc_ = dhmc(test)
-burn_in = 10 ** 1
-n_sample = 10 ** 1
+burn_in = 10
+n_sample = 10
 stepsize_range = [0.03,0.15]
 n_step_range = [10, 20]
-n_chain = 2
+n_chain = 3
 
-all_stats = dhmc_.sample_multiple_chains(n_chain, n_sample, burn_in=burn_in, stepsize_range=stepsize_range, n_step_range=n_step_range)
+all_stats = dhmc_.sample_multiple_chains(n_chain, n_sample, burn_in=burn_in, stepsize_range=stepsize_range, n_step_range=n_step_range, n_update = 5,save_samples = True)
 
 for i in all_stats:
     print('Chain {}: acceptance rate {} \n'.format(i, all_stats[i]['accept_rate'] ))
+
+### MCMC diagnotics:
+all_vars = list(all_stats[0]['samples'])
+mu_true = np.array([5.28])
+var_true = np.array([1.429**2])
+for key in all_vars:
+    _, n_dim =  all_stats[0]['samples'].as_matrix(columns=[key]).shape
+    for i in range(n_chain):
+        samples = np.empty(shape=(n_chain, n_sample, n_dim))
+        samples[i] = all_stats[i]['samples'].as_matrix(columns=[key])
+    ess = effective_sample_size(samples, mu_true, var_true)
+    ess_mc = effective_sample_size(samples)
+    r_hat = gelman_rubin_diagnostic(samples)
+    print('ess for {}: '.format(key), ess)
+    print('monte carlo ess for {}: '.format(key), ess_mc)
+    print('r value for '.format(key), r_hat)
 
 
 ### MCMC diagnotics:
@@ -38,3 +59,7 @@ for i in all_stats:
 # r_hat = gelman_rubin_diagnostic(x_)
 # r_truemean = gelman_rubin_diagnostic(x_, mu_true)
 # print('r value: ', r_hat, r_truemean)
+
+
+
+
