87385b9b1dd5155fc9ac13c56794b2849260a338
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-27 09:30:17 +0000

    added gamma model for testing

--- pyfo/unittests/models/gamma/run_gamma.py
+++ pyfo/unittests/models/gamma/run_gamma.py
@@ -7,3 +7,15 @@ Date created:  26/01/2018
 
 License: MIT
 '''
+
+from pyfo.pyfoppl.foppl import imports
+import gamma as test
+from pyfo.inference.dhmc import DHMCSampler as dhmc
+
+burn_in = 1
+n_samples = 1
+stepsize_range = [0.05, 0.25]
+n_step_range = [10, 20]
+# test.model.display_graph()
+dhmc_ = dhmc(test)
+stats = dhmc_.sample(n_samples, burn_in, stepsize_range, n_step_range, seed=123, print_stats=True)
