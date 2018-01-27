57a57f42a4a7f0325eab16efdcf0e6f969f0afcf
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-01-27 11:01:49 +0000

    minor changes to transformation flags

--- pyfo/distributions/exponential.py
+++ pyfo/distributions/exponential.py
@@ -18,7 +18,7 @@ class Exponential(TorchDistribution):
     Args:
         rate (float or Tensor or Variable): rate = 1 / scale of the distribution
     """
-    def __init__(self, rate):
+    def __init__(self, rate, transformed=False):
         self.rate =vc(rate)
         torch_dist = torch.distributions.Exponential(rate=self.rate)
         super(Exponential, self).__init__(torch_dist)
--- pyfo/distributions/gamma.py
+++ pyfo/distributions/gamma.py
@@ -7,12 +7,12 @@ from pyfo.distributions.log_gamma import LogGamma
 class Gamma(TorchDistribution):
     r"""
     """
-    def __init__(self, alpha, beta, Transformed=True):
+    def __init__(self, alpha, beta, transformed=True):
         self.alpha = vc(alpha)
         self.beta= vc(beta)
-        if Transformed:
+        if transformed:
             torch_dist = LogGamma(concentration=self.alpha, rate=self.beta)
-            super(Gamma, self).__init__(torch_dist, Transformed=Transformed)
+            super(Gamma, self).__init__(torch_dist, Transformed=transformed)
         else:
             torch_dist = torch.distributions.Gamma(concentration=self.alpha, rate=self.beta)
         super(Gamma, self).__init__(torch_dist)
