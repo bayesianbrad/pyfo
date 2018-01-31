the energy calculation is wrong!

pyfo/inference/dhmc.py
@@ -204,7 +204,7 @@ class DHMCSampler(object):
         :return: Tensor
         """
         if self._disc_keys is not None:
-            kinetic_disc = torch.sum(torch.stack([self.M * torch.dot(torch.abs(p[name]),torch.abs(p[name])) for name in self._disc_keys]))
+            kinetic_disc = torch.sum(torch.stack([self.M *torch.abs(p[name]) for name in self._disc_keys]))
         else:
             kinetic_disc = VariableCast(0)
         if self._cont_keys is not None:
@@ -212,7 +212,7 @@ class DHMCSampler(object):
         else:
             kinetic_cont = VariableCast(0)
         if self._if_keys is not None:
-            kinetic_if =  torch.sum(torch.stack([self.M * torch.dot(torch.abs(p[name]),torch.abs(p[name])) for name in self._if_keys]))
+            kinetic_if =  torch.sum(torch.stack([self.M *torch.abs(p[name]) for name in self._if_keys]))
         else:
             kinetic_if = VariableCast(0)
         kinetic_energy = kinetic_cont + kinetic_disc + kinetic_if