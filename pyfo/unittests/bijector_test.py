c09eed6da1bf4455e2540b209ec2f6480ef3da11
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-02-02 11:44:26 +0000

    playing around

--- pyfo/unittests/bijector_test.py
+++ pyfo/unittests/bijector_test.py
@@ -7,3 +7,12 @@ Date created:  01/02/2018
 
 License: MIT
 '''
+import torch
+from torch.autograd import Variable
+import torch.distributions.constraints as constraints
+from torch.distributions.constraint_registry import biject_to
+import torch.distributions as dists
+import torch.distributions.transforms as transforms
+from pyfo.utils.core import VariableCast as vc
+from torch.distributions import TransformedDistribution as td
+dist1 = dists.Exponential(vc(2))
