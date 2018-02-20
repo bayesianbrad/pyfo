bb0f3494746f0295f3fd4ac541ea1344371ed961
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-02-20 15:03:10 +0000

    added the categorical fail case and binomial fail cases

--- pyfo/unittests/models/categorical/run_cat.py
+++ pyfo/unittests/models/categorical/run_cat.py
@@ -10,17 +10,17 @@ License: MIT
 from pyfo.pyfoppl.foppl import imports
 import cat as test
 
-print(test.model)
-print(test.model.gen_pdf_code)
-# from pyfo.inference.dhmc import DHMCSampler as dhmc
+# print(test.model)
+# print(test.model.gen_pdf_code)
+from pyfo.inference.dhmc import DHMCSampler as dhmc
 
-# dhmc_ = dhmc(test)
-# burn_in = 2000
-# n_sample = 3500
-# stepsize_range = [0.01,0.1]
-# n_step_range = [10, 40]
-#
-# stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range,plot=True, print_stats=True, save_samples=True)
+dhmc_ = dhmc(test)
+burn_in = 2000
+n_sample = 2000
+stepsize_range = [0.01,0.1]
+n_step_range = [10, 40]
+
+stats = dhmc_.sample(n_samples=n_sample,burn_in=burn_in,stepsize_range=stepsize_range,n_step_range=n_step_range,plot=True, print_stats=True, save_samples=True)
 
 # samples =  stats['samples']
 # all_samples = stats['samples_wo_burin'] # type, panda dataframe
