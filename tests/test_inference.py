a99c2f30b60b28d8b6e351712cb1dc71762ff172
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-03-16 20:15:55 +0000

    finally got binomial model to run and now know how to get the other tests to work. To finish

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -15,39 +15,46 @@ import torch
 import time
 import numpy as np
 from pyfo.inference.dhmc import DHMCSampler as dhmc
+from pyfo.pyfoppl.foppl import imports
+from pyfo.pyfoppl.foppl import compilers
 
 # Global variables
 
 TEST_MODELS = ['bin', 'cat', 'gamma', 'gauss_1d', 'gmm_1d_a', 'gmm_1d_b','gmm_1d_c', 'hmm', 'if_1d','lr', 'nested_if', 'poisson']
 
-burn_in = 5000
-n_samples = 5000
+burn_in = 500
+n_samples = 500
 stepsize_range = [0.05, 0.18]
 n_step_range = [10, 20]
 seed= 123456
 
 class BinomialOneSuccessTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        class BinomialOneSuccess():
 
-
-    def __init__(self):
-        self.n = 1
-        self.p = 0.8
-        self.q = 1 - self.p
-        self.true_posterior_mean = 0.8
-        self.true_posterior_std = np.sqrt(0.16)
-
+            def __init__(self):
+                self.n = 1
+                self.p = 0.8
+                self.q = 1 - self.p
+                self.true_posterior_mean = 0.8
+                self.true_posterior_std = np.sqrt(0.16)
+                self.model_code = '(let [x1 (sample (binomial  1 [0.8]))]' \
+                             'x1)'
+                graph, expr = compilers.compile(self.model_code)
+                self.model = graph.create_model(result_expr=expr)
+                super().__init__()
+        self._model = BinomialOneSuccess()
+        super().__init__(*args,**kwargs)
 
     def test_run(self):
-        from pyfo.pyfoppl.foppl import imports
-        import bin as test
-        dhmc_ = dhmc(test)
+        dhmc_ = dhmc(self._model.model)
         stats = dhmc_.sample(chain_num=0, n_samples=n_samples, burn_in=burn_in, stepsize_range=stepsize_range,
                              n_step_range=n_step_range, seed=seed)
         self.samples = stats['samples']
         self.test_mean = np.mean(self.samples)
         self.test_std = np.std(self.samples)
-        self.assertAlmostEqual(self.true_posterior_mean, self.test_mean, msg='The binomial one trial inferred mean are too different',delta=0.04)
-        self.assertAlmostEqual(self.true_posterior_std, self.test_std, msg='The binomial one trial inferred std are too different', delta=0.04)
+        self.assertAlmostEqual(self._model.true_posterior_mean, self.test_mean, msg='The binomial one trial inferred mean are too different',delta=0.04)
+        self.assertAlmostEqual(self._model.true_posterior_std, self.test_std, msg='The binomial one trial inferred std are too different', delta=0.04)
 
 # @unittest.skip('Not working')
 # class Categorical1dTestCase(unittest.TestCase):
@@ -79,26 +86,26 @@ class BinomialOneSuccessTestCase(unittest.TestCase):
 #
 #
 #
-# class ConjugateGaussianTestCase(unittest.TestCase):
-#
-#
-#     def __init__(self):
-#         self.true_posterior_mean = 5.5
-#         # self.true_posterior_std =
-#
-#
-#     def testrun(self):
-#         from pyfo.pyfoppl.foppl import imports
-#         import gauss_1d as test
-#         dhmc_ = dhmc(test)
-#         stats = dhmc_.sample(chain_num=0, n_samples=n_samples, burn_in=burn_in, stepsize_range=stepsize_range,
-#                              n_step_range=n_step_range, seed=seed)
-#         self.samples = stats['samples']
-#         self.test_mean = np.mean(self.samples)
-#         self.test_std = np.std(self.samples)
-#         self.assertAlmostEqual(self.true_posterior_mean, self.test_mean, msg='The Gaussian 1d trial inferred mean are too different',delta=0.04)
-#         # self.assertAlmostEqual(self.true_posterior_std, self.test_std, msg='The binomial one trial inferred std are too different', delta=0.04)
-#
+class ConjugateGaussianTestCase(unittest.TestCase):
+
+
+    def __init__(self):
+        self.true_posterior_mean = 5.5
+        # self.true_posterior_std =
+
+
+    def testrun(self):
+        from pyfo.pyfoppl.foppl import imports
+        import gauss_1d as test
+        dhmc_ = dhmc(test)
+        stats = dhmc_.sample(chain_num=0, n_samples=n_samples, burn_in=burn_in, stepsize_range=stepsize_range,
+                             n_step_range=n_step_range, seed=seed)
+        self.samples = stats['samples']
+        self.test_mean = np.mean(self.samples)
+        self.test_std = np.std(self.samples)
+        self.assertAlmostEqual(self.true_posterior_mean, self.test_mean, msg='The Gaussian 1d trial inferred mean are too different',delta=0.04)
+        # self.assertAlmostEqual(self.true_posterior_std, self.test_std, msg='The binomial one trial inferred std are too different', delta=0.04)
+
 if __name__ == '__main__':
     tests = []
     tests.append('BinomialOneSuccessTestCase')
