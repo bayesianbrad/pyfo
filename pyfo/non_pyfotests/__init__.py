de4cea8a8418289753ad029a4b678032822bb44b
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-03-20 17:29:08 +0000

    clean up

--- pyfo/non_pyfotests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-'''
-Author: Bradley Gram-Hansen
-Time created:  11:24
-Date created:  08/03/2018
-
-License: MIT
-'''
--- pyfo/non_pyfotests/tsting_bijectors.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-from collections import OrderedDict
-
-import pyro
-import pyro.distributions as dist
-from pyro.infer.mcmc.trace_kernel import TraceKernel
-
-
