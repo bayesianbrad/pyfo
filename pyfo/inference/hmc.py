4d03002a5443867dbafe546bf0953d1a9ae063ab
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-04-13 05:31:37 +0100

    added adaptive step size and acceptance ratio

--- pyfo/inference/hmc.py
+++ pyfo/inference/hmc.py
@@ -16,7 +16,9 @@ import numpy as np
 import pandas as pd
 import torch
 import copy
+import torch.distributions as dists
 from torch.autograd import Variable
+from torch.distributions import constraints
 # the state interacts with the interface, where ever that is placed....
 from pyfo.utils import state
 from pyfo.utils.core import VariableCast
@@ -24,6 +26,7 @@ from pyfo.utils.eval_stats import extract_stats
 from pyfo.utils.eval_stats import save_data
 from pyfo.utils.plotting import Plotting as plot
 from pyfo.inference.mcmc import MCMC
+from pyfo.utils.core import DualAveraging
 
 
 class HMC(MCMC):
@@ -55,18 +58,83 @@ class HMC(MCMC):
            self.step_range = self.step_size * num_steps
        else:
            self.step_range = 2* math.pi
-        self.num_steps = max(1, int(self.num_steps / self.step_size))
+       self.num_steps = max(1, int(self.num_steps / self.step_size))
 
        self.adapt_step_size = adapt_step_size
        self._target_accept_prob = 0.8
 
        self.transforms = {} if transforms is not None else False
        self.automatic_transformed_enbaled = True if transforms is None else False
-       self.initialize()
+       self.initialize()w
        self.generate_latent_vars()
 
        super(HMC,self).__init__()
 
+    def _find_reasonable_step_size(self, state):
+        step_size = self.step_size
+        # NOTE: This target_accept_prob is 0.5 in NUTS paper, is 0.8 in Stan,
+        # and is different to the target_accept_prob for Dual Averaging scheme.
+        # We need to discuss which one is better.
+        target_accept_logprob = math.log(self._target_accept_prob)
+
+        # We are going to find a step_size which make accept_prob (Metropolis correction)
+        # near the target_accept_prob. If accept_prob:=exp(-delta_energy) is small,
+        # then we have to decrease step_size; otherwise, increase step_size.
+        p = self.momentum_sample()
+        energy_current = self._energy(state, p)
+        state_new, p_new, potential_energy = self.leapfrog_step(state, p, self._potential_energy(state), step_size)
+        energy_new = potential_energy + self._kinetic_energy(p_new)
+        delta_energy = energy_new - energy_current
+        # direction=1 means keep increasing step_size, otherwise decreasing step_size
+        direction = 1 if target_accept_logprob < -delta_energy else -1
+
+        # define scale for step_size: 2 for increasing, 1/2 for decreasing
+        step_size_scale = 2 ** direction
+        direction_new = direction
+        # keep scale step_size until accept_prob crosses its target
+        # TODO: make thresholds for too small step_size or too large step_size
+        while direction_new == direction:
+            step_size = step_size_scale * step_size
+            state_new, p_new, potential_energy = self._leapfrog_step(
+                state, p, self._potential_energy(state), step_size)
+            energy_new = potential_energy + self._kinetic_energy(p_new)
+            delta_energy = energy_new - energy_current
+            direction_new = 1 if target_accept_logprob < -delta_energy else -1
+        return step_size
+
+    def _adapt_step_size(self, accept_prob):
+        # calculate a statistic for Dual Averaging scheme
+        H = self._target_accept_prob - accept_prob
+        self._adapted_scheme.step(H)
+        log_step_size, _ = self._adapted_scheme.get_state()
+        self.step_size = math.exp(log_step_size)
+        self.num_steps = max(1, int(self.trajectory_length / self.step_size))
+
+    def setup(self, state):
+
+        for name, node in sorted(trace.iter_stochastic_nodes(), key=lambda x: x[0]):
+            r_loc = torch.zeros_like(node["value"])
+            r_scale = torch.ones_like(node["value"])
+            self._r_dist[name] = dist.Normal(loc=r_loc, scale=r_scale)
+            if node["fn"].support is not constraints.real and self._automatic_transform_enabled:
+                self.transforms[name] = biject_to(node["fn"].support).inv
+        self._validate_trace(trace)
+
+        if self.adapt_step_size:
+            for name, transform in self.transforms.items():
+                state[name] = transform(state[name])
+            self.step_size = self._find_reasonable_step_size(state)
+            self.num_steps = max(1, int(self.trajectory_length / self.step_size))
+            # make prox-center for Dual Averaging scheme
+            loc = math.log(10 * self.step_size)
+            self._adapted_scheme = DualAveraging(prox_center=loc)
+
+    def end_warmup(self):
+        if self.adapt_step_size:
+            self.adapt_step_size = False
+            _, log_step_size_avg = self._adapted_scheme.get_state()
+            self.step_size = math.exp(log_step_size_avg)
+            self.num_steps = max(1, int(self.trajectory_length / self.step_size))
     def _kinetic(self, p):
         """
 
@@ -104,47 +172,51 @@ class HMC(MCMC):
 
     def momentum_sample(self):
         """
-        Constructs a momentum dictionary where for the discrete keys we have laplacian momen tum
+        Constructs a momentum dictionary for contin
         and for continous keys we have gaussian
         :return:
         """
         p = {}
-        VariableCast(self.M * np.random.randn(self._sample_sizes[key]))
         for key in self._cont_keys:
             p[key] = VariableCast(self.M * np.random.randn(self._sample_sizes[key]))
         return p
-    
+
     def sample(self):
         '''
 
         :return:
         '''
 
-        # automatically transform `z` to unconstrained space, if needed.
+        # automatically transform `state` to unconstrained space, if needed.
         for key, transform in self.transforms.items():
             state[key] = transform(state[key])
         p = self.momentum_sample()
-        state_new, p_new = velocity_verlet(z, r,
+        state_new, p_new = velocity_verlet(state, p,
                                        self._potential_energy,
                                        self.step_size,
                                        self.num_steps)
         # apply Metropolis correction.
-        energy_proposal = self._energy(z_new, r_new)
-        energy_current = self._energy(z, r)
+        energy_proposal = self._energy(state_new, p_new)
+        energy_current = self._energy(state, p)
         delta_energy = energy_proposal - energy_current
         rand = pyro.sample('rand_t='.format(self._t), dist.Uniform(ng_zeros(1), ng_ones(1)))
-        if rand.log() < -delta_energy:
+        if rand < (-delta_energy).exp():
             self._accept_cnt += 1
-            z = z_new
+            state = state_new
+
+        if self.adapt_step_size:
+            accept_prob = (-delta_energy).exp().clamp(max=1).item()
+            self._adapt_step_size(accept_prob)
+
         self._t += 1
 
-        # get trace with the constrained values for `z`.
+        # get trace with the constrained values for `state`.
         for key, transform in self.transforms.items():
-            z[key] = transform.inv(z[key])
-        return self._get_trace(z)
+            state[key] = transform.inv(state[key])
+        return self._get_trace(state)
         return 0
 
-    def gauss_laplace_leapfrog(self, x0, p0, stepsize):
+    def _leapfrog_step(self, x0, p0, stepsize):
         """
         Performs the full DHMC update step. It updates the continous parameters using
         the standard integrator and the discrete parameters via the coordinate wie integrator.
@@ -175,6 +247,6 @@ class HMC(MCMC):
         logp = self.__generate_pdf(x, set_leafs=True)
         for key in self._cont_keys:
             p[key] = p[key] + 0.5*stepsize*self.____grad_logp(logp,x[key])
-        return x, p, n_feval, n_fupdate, 0
+        return x, p, logp
 
 
