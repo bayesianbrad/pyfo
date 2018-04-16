648f6a1b6df6981e98b22c7f9f30d3f3c78ce166
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-04-16 23:58:24 +0100

    added plotting script and updated model code

--- pyfo/unittests/models/ifmodels/if_1d_models/data2018-04-16T18:05:05.368021/plot_hist.py
+++ pyfo/unittests/models/ifmodels/if_1d_models/data2018-04-16T18:05:05.368021/plot_hist.py
@@ -7,3 +7,26 @@ Date created:  16/04/2018
 
 License: MIT
 '''
+import seaborn as sns
+import pandas as pd
+from  matplotlib import pyplot as plt
+import numpy as np
+x = pd.read_csv('sppl_samples_after_burnin.csv')
+x = x.as_matrix()
+# sns.distplot(x, kde=True)
+# plt.figure(figsize=(10, 4))
+# plt.hist(x['z'], bins='auto', normed=1)
+num_bins = 100
+
+fig, ax = plt.subplots()
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, normed=True)
+
+# add a 'best fit' line
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of SPPL inferred density')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.savefig('sppl_7.pdf')
+print('Completed plots')
--- pyfo/unittests/models/ifmodels/if_1d_models/data2018-04-16T19:47:09.330855/plot_hist.py
+++ pyfo/unittests/models/ifmodels/if_1d_models/data2018-04-16T19:47:09.330855/plot_hist.py
@@ -7,3 +7,35 @@ Date created:  16/04/2018
 
 License: MIT
 '''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Author: Bradley Gram-Hansen
+Time created:  18:16
+Date created:  16/04/2018
+
+License: MIT
+'''
+import seaborn as sns
+import pandas as pd
+from  matplotlib import pyplot as plt
+import numpy as np
+x = pd.read_csv('sppl_samples_after_burnin.csv')
+x = x.as_matrix()
+# sns.distplot(x, kde=True)
+# plt.figure(figsize=(10, 4))
+# plt.hist(x['z'], bins='auto', normed=1)
+num_bins = 100
+
+fig, ax = plt.subplots()
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, normed=True)
+
+# add a 'best fit' line
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of SPPL inferred density')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.savefig('sppl_025.pdf')
+print('Completed plots')
