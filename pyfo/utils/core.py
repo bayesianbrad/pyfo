9200e42123567325f9ccb48cabdf050751437c44
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-05-03 23:19:22 -0700

    added plot graphical model

--- pyfo/utils/core.py
+++ pyfo/utils/core.py
@@ -12,6 +12,14 @@ import numpy as np
 import torch.distributions as dists
 from torch.distributions import constraints, biject_to
 
+try:
+    import networkx as nx
+except ModuleNotFoundError:
+    nx = None
+try:
+    import matplotlib.pyplot as plt
+except ModuleNotFoundError:
+    plt = None
 
 class DualAveraging(object):
     """
@@ -78,6 +86,66 @@ class DualAveraging(object):
         :math:`\left\{x_i\right\}_{i=1}^t` in primal space.
         """
         return self._x_t, self._x_avg
+
+
+def create_network_graph(vertices):
+    """
+
+    Create a `networkx` graph. Used by the method `display_graph()`.
+
+    :param vertices :type set :descript a collection of vertices that form the graph
+    :return: Either a `networkx.DiGraph` instance or `None`.
+    """
+    if nx:
+        G = nx.DiGraph()
+        for v in vertices:
+            G.add_node(v.display_name)
+            for a in v.ancestors:
+                G.add_edge(a.display_name, v.display_name)
+        return G
+    else:
+        return None
+
+def display_graph(vertices):
+    """
+    Transform the graph to a `networkx.DiGraph`-structure and display it using `matplotlib` -- if the necessary
+    libraries are installed.
+
+    :return: `True` if the graph was drawn, `False` otherwise.
+    """
+    G = create_network_graph()
+    if nx and plt and G:
+        try:
+            from networkx.drawing.nx_agraph import graphviz_layout
+            pos = graphviz_layout(G, prog='dot')
+        except ModuleNotFoundError:
+            from networkx.drawing.layout import shell_layout
+            pos = shell_layout(G)
+        except ImportError:
+            from networkx.drawing.layout import shell_layout
+            pos = shell_layout(G)
+        plt.subplot(111)
+        plt.axis('off')
+        nx.draw_networkx_nodes(G, pos,
+                               node_color='r',
+                               node_size=1250,
+                               nodelist=[v.display_name for v in vertices if v.is_sampled])
+        nx.draw_networkx_nodes(G, pos,
+                               node_color='b',
+                               node_size=1250,
+                               nodelist=[v.display_name for v in vertices if v.is_observed])
+        for v in vertices:
+            nx.draw_networkx_edges(G, pos, arrows=True,
+                                   edgelist=[(a.display_name, v.display_name) for a in v.dist_ancestors])
+            nx.draw_networkx_edges(G, pos, arrows=True,
+                                   style='dashed',
+                                   edge_color='g',
+                                   edgelist=[(a.display_name, v.display_name) for a in v.cond_ancestors])
+        nx.draw_networkx_labels(G, pos, font_color='w', font_weight='bold')
+        plt.show()
+        return True
+    else:
+        return False
 
 def VariableCast(value, grad = False, dist=None):
     '''casts an input to torch.tensor object
